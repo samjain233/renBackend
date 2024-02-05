9e30994795546ef717eb94ed00579a72d1369d11
samjain233 <94921996+samjain233@users.noreply.github.com>
2024-02-05 17:41:32 +0530

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import connectDB from "./db/conn.js";
+import cors from "cors";
 
 connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
